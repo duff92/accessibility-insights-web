9bdd3e3e8759e0bb5f8d52833237ae76ff74a3a2
RobGallo <43080011+RobGallo@users.noreply.github.com>
2019-09-30 13:11:28 -0400

    Create CardSelectionStore interface

---
+++ src/background/actions/card-selection-actions.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import { Action } from '../../common/flux/action';
+import { UnifiedScanCompletedPayload } from '../actions/action-payloads';
+
+export class CardSelectionActions {
+    public readonly toggleRuleExpandCollapse = new Action();
+    public readonly toggleCardSelection = new Action();
+    public readonly collapseAllRules = new Action();
+    public readonly scanCompleted = new Action<UnifiedScanCompletedPayload>();
+    public readonly getCurrentState = new Action();
+}
